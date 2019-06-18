f8cd2078f9274a4a00be50fd73bbc3070d1edeec
Everett Smith <evepsmith15@gmail.com>
2019-06-18 15:06:47 -0700

    added script

--- assets/javascript/jobsearch.js
+++ assets/javascript/jobsearch.js
@@ -210,22 +210,22 @@ function initMap(Starbucks, Bothell, WA) { //this is just an example. Will remov
     zoom: 3, //highest value is 14
     center: { lat: 42, lng: -70 }//latitude and longitude go here. 
   }
+
   //creates the map
   var map = new google.maps.Map(document.getElementById('map'), options);
   google.maps.event.addListener(map, 'click',
     function (event) {
       addMarker({ name, city, state });
     });
+
   //creates the marker
-  markers = [
-    addMarker({ name, city, state })
-  ];
+  var markers = addMarker({Starbucks, Bothell, WA});
 
   for (var i = 0; i < markers.length; i++) { //for every new location given, add a marker
     addMarker([i]);
   }
 
-  function addMarker(coords) { //coords is whatever variable is used in searchAPI
+  function addMarker() { //coords is whatever variable is used in searchAPI
     var marker = new google.maps.Marker({
       position: props.coords,
       map: map,
