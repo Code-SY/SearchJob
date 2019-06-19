dd428bb92d53f23a874a5fa471a447173baa8d48
Code-SY <syanina@gmail.com>
2019-06-19 11:02:14 -0700

    changes

--- assets/javascript/jobsearch.js
+++ assets/javascript/jobsearch.js
@@ -1,43 +1,50 @@
 
 // app.js
 $(document).ready(function () {
-  $("#search").click(function (evt) {
+  $("#searchBtn").click(function (evt) {
       evt.preventDefault();
+
       var searchTerms = $("#searchTerms").val();
+      var searchLocation = $("#searchLocation").val();
+      
       var criteria = {
-          keywords: "javascript, php, ruby",
-          location: ""
+          keywords: searchTerms,
+          location: searchLocation
       };
 
-    searchJobs(criteria, showOnMapHandler);
+      searchJobs(criteria, showOnMapHandler);
   });
 });
+
 
 // Map integration point, this handler is attached to
 // click event on job posting area.
 // Parameter job has all available data for job posting
-var showOnMapHandler = function (job) {
+var showOnMapHandler = function(job) {
   $(".job-selected").removeClass("job-selected");
 
   $(this).addClass("job-selected");
   $("#job-description").html(job.description);
+  $("#job-description").dialog({
+      title: job.title,
+      width: 600,
+      height: 500,
+      closeOnEscape: true,
+      resizable: true,
+      classes: {
+          "ui-dialog": "modal-content",
+          "ui-dialog-titlebar": "modal-header",
+          "ui-dialog-title": "modal-title",
+          "ui-dialog-content": "modal-body"
+      }
+  });
 
   console.log(job);
 }
 // searchAPI js file
-//***********Added the searchAPI.js to index.html */
-// Moved to app.js
-//$(document).ready(function () {
-//    $("#search").click(function () {
-//        searchJobs(criteria, clickHandler);
-//    });
-//});
-//var criteria = {
-//    keywords: "javascript, php, ruby",
-//    location: ""
-//}
-
+// *************Why the name containerID?
 var containerId = "#search-results";
+console.log(containerId);
 var searchJobs = function (searchCriteria, clickHandler) {
   // Validate input
   if (searchCriteria.keywords === "") {
@@ -90,7 +97,7 @@ var searchJobs = function (searchCriteria, clickHandler) {
         location = location.split(" or ")[0];
         location = location.replace(/"/gm, "");
       }
-
+ 
       var job = {
         id: searchItem.id,
         title: searchItem.title,
@@ -164,9 +171,9 @@ function mergeObjects(obj1, obj2) {
 
   return result;
 }
-//stops searchAPI file//
+//end searchAPI file//
 
-
+//--------------------------
 // map.js file
 var queryMap = "https://maps.googleapis.com/maps/api/js?";
 
